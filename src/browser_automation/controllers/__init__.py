c6646821c46172e971fc999b5c750ef617bb4331
Joe Van Os <joev@squareup.com>
2025-03-21 14:13:54 -0700

    Fixed Python package structure
    - Added controllers/__init__.py to properly expose controllers
    - Updated imports to use package structure
    - Added version to main __init__.py

---
+++ src/browser_automation/controllers/__init__.py
@@ -0,0 +1,5 @@
+"""Browser automation controllers."""
+from .browser_controller import BrowserController
+from .square_controller import SquareController
+
+__all__ = ['BrowserController', 'SquareController']
